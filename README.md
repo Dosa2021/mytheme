a786efff9c68c243877e7fcdd00debbe5beb9281
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-05-09 05:25:00 -0700

    css読み込み

---
+++ README.md
@@ -0,0 +1,10 @@
+・cssが読み込まれない
+
+x
+<link rel="stylesheet" href="style.css">
+
+o get_template_directory_uri() の呼び出しが必要
+<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
+
+参考
+https://qiita.com/9ever/items/2493afd47b73a5ba1827#cssjs%E7%94%BB%E5%83%8F%E3%81%AE%E5%8F%82%E7%85%A7%E5%85%88%E3%82%92%E4%BF%AE%E6%AD%A3%E3%81%99%E3%82%8B
--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>サンプルHTMLページ</title>
-</head>
-<body>
-    <h1>ようこそ！</h1>
-    <p>これはサンプルのHTMLページです。</p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>MyTheme</title>
+        <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
+    </head>
+    <body>
+        <header>
+            <ul>
+                <li>
+                    <a href="#">HOME ho</a>
+                </li>
+                <li>
+                    <a href="#">会社概要</a>
+                </li>
+            </ul>
+        </header>
+    </body>
 </html>
