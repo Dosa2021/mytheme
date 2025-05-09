3877d3b707ff4c8a92d63f8b8e0ed6d16eb304dd
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-05-09 06:21:09 -0700

    css追加

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <title>MyTheme</title>
+        <!-- reset.css destyle -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
         <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
     </head>
     <body>
