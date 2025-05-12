f896155fe273af7c4a819210f3dc762c1ec7bfed
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-05-11 19:39:18 -0700

    記事投稿機能実装

---
+++ functions.php
@@ -0,0 +1,3 @@
+<?php
+// 投稿時のアイキャッチ設定
+add_theme_support('post-thumbnails');
--- index.php
+++ index.php
@@ -22,78 +22,20 @@
             <h1>TATSUYA SUGAWARA</h1>
             <div class="pickup">
                 <ul>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
-                    <li>
-                        <img src="<?= get_template_directory_uri(); ?>/img/main_nagano3.jpg" alt="">
-                        <h2>Title</h2>
-                        <p>hogehoge</p>
-                        <a href="#">続きを読む</a>
-                    </li>
+                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                        <li>
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <?php the_post_thumbnail(); ?>
+                            <?php else : ?>
+                                <p>サムネイルがないです</p>
+                            <?php endif; ?>
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_content(); ?></p>
+                            <a href="#">続きを読む</a>
+                        </li>
+　                      <?php endwhile; else : ?>
+　　                    <p>記事がありません。</p>
+                    <?php endif; ?>
                 </ul>
             </div>
         </main>
