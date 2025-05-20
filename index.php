e935f2a13a87980be292febeb28894e955169534
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-05-19 21:55:29 -0700

    トップページには特定のタグ付した投稿のみを表示

--- index.php
+++ index.php
@@ -3,19 +3,23 @@
     <h1>TATSUYA SUGAWARA</h1>
     <div class="pickup">
         <ul>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <li>
-                    <?php if ( has_post_thumbnail() ) : ?>
-                        <?php the_post_thumbnail(); ?>
-                    <?php else : ?>
-                        <p>サムネイルがないです</p>
-                    <?php endif; ?>
-                    <h2><?php the_title(); ?></h2>
-                    <p><?php the_content(); ?></p>
-                    <a href="<?php the_permalink(); ?>">続きを読む</a>
-                </li>
-                    <?php endwhile; else : ?>
-                    <p>記事がありません。</p>
+            <?php $the_query = new WP_Query( array( 'tag' => 'pick_up' ) ); ?>
+            <?php if ( $the_query->have_posts() ) : ?>
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <?php the_post_thumbnail(); ?>
+                            <?php else : ?>
+                                <p>サムネイルがないです</p>
+                            <?php endif; ?>
+                            <h2><?php the_title(); ?></h2>
+                        </a>
+                    </li>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php else : ?>
+                <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
             <?php endif; ?>
         </ul>
     </div>
