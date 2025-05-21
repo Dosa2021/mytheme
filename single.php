22b8574b1ace9209bd9680ecb1d41978baeb6fb4
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-05-21 03:15:07 -0700

    投稿ページのデザイン修正

--- single.php
+++ single.php
@@ -1,11 +1,15 @@
 <?php get_header(); ?>
 <div class="single">
+    <div class="image-box">
+        <div class="shadow">
+            <?php if ( has_post_thumbnail() ) : ?>
+                <?php the_post_thumbnail(); ?>
+            <?php else : ?>
+                <p>サムネイルがないです</p>
+            <?php endif; ?>
+        </div>
+    </div>
     <h2><?php the_title(); ?></h2>
-    <?php if ( has_post_thumbnail() ) : ?>
-        <?php the_post_thumbnail(); ?>
-    <?php else : ?>
-        <p>サムネイルがないです</p>
-    <?php endif; ?>
-    <p><?php the_content(); ?></p>
+    <?php the_content(); ?>
 </div>
 <?php get_footer(); ?>
