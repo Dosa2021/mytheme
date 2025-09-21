2776f8da8a622abad5f878f52bb65b60858c552c
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-09-21 02:07:33 -0700

    テーマ名変更

--- header.php
+++ header.php
@@ -3,11 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>MyTheme</title>
+        <title>TATSUYA SUGAWARA</title>
         <!-- reset.css destyle -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?11">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.0/css/all.css">
+        <link rel="shortcut icon" href="https://dosaken.org/wp-content/uploads/2025/09/cottage.png">
         <script src="<?php echo get_template_directory_uri(); ?>/main.js" type="text/javascript" charset="utf-8"></script>
     </head>
     <body>
